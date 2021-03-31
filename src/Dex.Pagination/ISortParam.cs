2d7529ed3a823f803ede26ddbe4700d38295b473
Андрей Мошняга <a.moshnyaga@dextechnology.com>
2021-03-31 19:52:37 +0300

    feat: add queryable sorting by params

---
+++ src/Dex.Pagination/ISortParam.cs
@@ -0,0 +1,8 @@
+﻿namespace Dex.Pagination
+{
+    public interface ISortParam
+    {
+        string FieldName { get; set; }
+        bool IsDesc { get; set; }
+    }
+}
--- src/Dex.Pagination/PaginationQueryExtensions.cs
+++ src/Dex.Pagination/PaginationQueryExtensions.cs
@@ -26,8 +26,45 @@ namespace Dex.Pagination
                 throw new ArgumentNullException(nameof(source));
             if (pageFilter == null)
                 throw new ArgumentNullException(nameof(pageFilter));
-            
+
             return source.FilterPage(pageFilter.Page, pageFilter.PageSize);
+        }
+
+        public static IQueryable<T> OrderByParams<T>(this IQueryable<T> source, ISortParam sortParam)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (sortParam == null)
+                throw new ArgumentNullException(nameof(sortParam));
+
+            return source.OrderByParams(new[] {sortParam});
+        }
+
+        public static IQueryable<T> OrderByParams<T>(this IQueryable<T> source, ISortParam[] sortParams)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (sortParams == null || !sortParams.Any())
+                throw new ArgumentNullException(nameof(sortParams));
+
+            var selector = BuildSelector<T>(sortParams[0].FieldName);
+            var result = sortParams[0].IsDesc ? source.OrderByDescending(selector) : source.OrderBy(selector);
+
+            for (var i = 1; i < sortParams.Length; i++)
+            {
+                selector = BuildSelector<T>(sortParams[i].FieldName);
+                result = sortParams[i].IsDesc ? result.ThenByDescending(selector) : result.ThenBy(selector);
+            }
+
+            return result;
+        }
+
+        private static Expression<Func<TSource, IComparable>> BuildSelector<TSource>(string fieldName)
+        {
+            var param = Expression.Parameter(typeof(TSource));
+            var member = fieldName.Split('.').Aggregate<string, Expression>(param, Expression.Property);
+
+            return Expression.Lambda<Func<TSource, IComparable>>(Expression.Convert(member, typeof(IComparable)), param);
         }
     }
 }
