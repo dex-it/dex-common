0f7a54435941222c4eb6536b2c8ad6c91da75207
mmx003 <mmx003@gmail.com>
2021-04-13 23:28:44 +0300

    RabbitMqOptions - ToString() - to connection string

--- src/Dex.MassTransit.Rabbit/RabbitMqOptions.cs
+++ src/Dex.MassTransit.Rabbit/RabbitMqOptions.cs
@@ -2,11 +2,17 @@
 {
     public class RabbitMqOptions
     {
+        public bool IsSecure { get; set; } = false;
         public string Host { get; set; } = "localhost";
         public ushort Port { get; set; } = 5672;
         public string VHost { get; set; } = "/";
-        
         public string Username { get; set; } = "guest";
         public string Password { get; set; } = "guest";
+
+        public override string ToString()
+        {
+            var schema = IsSecure ? "amqps" : "amqp";
+            return $"{schema}://{Username}:{Password}@{Host}:{Port}{VHost}";
+        }
     }
 }
