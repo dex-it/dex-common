cb9b0c78138942f6acbba6c3123371681f7bbee8
mmx003 <mmx003@gmail.com>
2021-06-03 23:50:18 +0300

    add dex.cap solution

---
+++ src/Dex.Cap/Dex.Cap.Outbox.Neo4j/Neo4JOutboxDataProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Dex.Cap.Outbox.Models;
+using Dex.Cap.Outbox.Options;
+using Microsoft.Extensions.Options;
+using Neo4jClient.Transactions;
+
+namespace Dex.Cap.Outbox.Neo4j
+{
+    public class Neo4JOutboxDataProvider<TDbContext> : BaseOutboxDataProvider<TDbContext>
+    {
+        private readonly ITransactionalGraphClient _graphClient;
+        private readonly OutboxOptions _outboxOptions;
+
+        public Neo4JOutboxDataProvider(ITransactionalGraphClient graphClient, IOptions<OutboxOptions> outboxOptions)
+        {
+            _graphClient = graphClient ?? throw new ArgumentNullException(nameof(graphClient));
+            _outboxOptions = outboxOptions?.Value ?? throw new ArgumentNullException(nameof(outboxOptions));
+        }
+
+        public override async Task<Models.Outbox> Save(Models.Outbox outbox)
+        {
+            await _graphClient.Cypher
+                .Create($"(outbox:{nameof(Outbox)})")
+                .Set("outbox = {outbox}").WithParam("outbox", outbox)
+                .ExecuteWithoutResultsAsync();
+
+            return outbox;
+        }
+
+        public override async Task<Models.Outbox[]> GetWaitingMessages()
+        {
+            const OutboxMessageStatus failedStatus = OutboxMessageStatus.Failed;
+            const OutboxMessageStatus newStatus = OutboxMessageStatus.New;
+
+            var outboxes = await _graphClient.Cypher.Match($"(o:{nameof(Outbox)})")
+                .Where((Models.Outbox o) => o.Retries < _outboxOptions.Retries)
+                .AndWhere((Models.Outbox o) => o.Status == failedStatus || o.Status == newStatus)
+                .Return<Models.Outbox>("o")
+                .OrderBy("o.Created")
+                .Limit(_outboxOptions.MessagesToProcess)
+                .ResultsAsync;
+
+            return outboxes.ToArray();
+        }
+
+        protected override async Task UpdateOutbox(Models.Outbox outbox)
+        {
+            await _graphClient.Cypher.Merge($"(o:{nameof(Outbox)} {{ Id: {{id}} }})")
+                .WithParam("id", outbox.Id)
+                .Set("o = {outbox}").WithParam("outbox", outbox)
+                .ExecuteWithoutResultsAsync();
+        }
+    }
+}
