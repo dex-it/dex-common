2cf7cd047f35f0194a92041f352a3b37b6574b6a
illusionist <illusionist.nemo@gmail.com>
2021-06-11 09:48:02 +0300

    add DataTransactionScopeProviderTests.cs

---
+++ src/DataProvider/Dex.DataProvider.Tests/DataTransactionScopeProviderTests.cs
@@ -0,0 +1,47 @@
+using System.Transactions;
+using Dex.DataProvider.Providers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dex.DataProvider.Tests
+{
+    public class DataTransactionScopeProviderTests
+    {
+        public DataTransactionScopeProviderTests()
+        {
+            _dataTransactionScopeProvider = new DataTransactionScopeProvider();
+        }
+
+        [Test]
+        public void TransactionTest()
+        {
+            var dataTransaction = _dataTransactionScopeProvider.Transaction();
+
+            dataTransaction.Should().NotBeNull();
+            var currentLevel = Transaction.Current?.IsolationLevel;
+            currentLevel.Should().NotBeNull();
+            currentLevel.Should().Be(IsolationLevel.ReadCommitted);
+        }
+        
+        [TestCase(IsolationLevel.Serializable)]
+        [TestCase(IsolationLevel.RepeatableRead)]
+        [TestCase(IsolationLevel.ReadCommitted)]
+        [TestCase(IsolationLevel.ReadUncommitted)]
+        [TestCase(IsolationLevel.Snapshot)]
+        [TestCase(IsolationLevel.Chaos)]
+        public void NotCurrentTransactionTest(IsolationLevel isolationLevel)
+        {
+            var startLevel = Transaction.Current?.IsolationLevel;
+            startLevel.Should().BeNull();
+            
+            var dataTransaction = _dataTransactionScopeProvider.Transaction(isolationLevel);
+
+            dataTransaction.Should().NotBeNull();
+            var currentLevel = Transaction.Current?.IsolationLevel;
+            currentLevel.Should().NotBeNull();
+            currentLevel.Should().Be(isolationLevel);
+        }
+
+        private readonly DataTransactionScopeProvider _dataTransactionScopeProvider;
+    }
+}
