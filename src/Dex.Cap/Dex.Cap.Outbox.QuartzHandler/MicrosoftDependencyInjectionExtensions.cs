72a3d08b590e77594d88a57f93a10ae47305f5f3
mmx003 <mmx003@gmail.com>
2021-07-03 10:36:55 +0300

    add Quartz routine for OutboxHandler job

---
+++ src/Dex.Cap/Dex.Cap.Outbox.QuartzHandler/MicrosoftDependencyInjectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace Dex.Cap.Outbox.QuartzHandler
+{
+    public static class MicrosoftDependencyInjectionExtensions
+    {
+        public static void RegisterOutboxInQuartz(this IServiceCollection services, int periodSeconds = 30)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddQuartz(q =>
+            {
+                q.UseMicrosoftDependencyInjectionJobFactory();
+                q.UseInMemoryStore();
+
+                // job OutboxJob
+                q.AddJob<OutboxHandlerJob>(c =>
+                {
+                    c.StoreDurably();
+                    c.RequestRecovery();
+                });
+                q.ScheduleJob<OutboxHandlerJob>(c =>
+                {
+                    c.StartNow()
+                        .WithSimpleSchedule(b => { b.WithInterval(TimeSpan.FromSeconds(periodSeconds)).RepeatForever(); });
+                });
+            });
+            services.AddQuartzServer(options =>
+            {
+                // when shutting down we want jobs to complete gracefully
+                options.WaitForJobsToComplete = true;
+            });
+        }
+    }
+}
---
+++ src/Dex.Cap/Dex.Cap.Outbox.QuartzHandler/OutboxHandlerJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Dex.Cap.Outbox.QuartzHandler
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal sealed class OutboxHandlerJob : IJob
+    {
+        private readonly IOutboxHandler _outboxHandler;
+
+        public OutboxHandlerJob(IOutboxHandler outboxHandler)
+        {
+            _outboxHandler = outboxHandler ?? throw new ArgumentNullException(nameof(outboxHandler));
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            return _outboxHandler.Process(context.CancellationToken);
+        }
+    }
+}
