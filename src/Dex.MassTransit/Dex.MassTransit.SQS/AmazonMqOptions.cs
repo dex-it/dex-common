4cb1191919416fb71c5f9b523ec1baac3a48f4b4
Vitaliy Danilov <vitaliy931@gmail.com>
2021-07-30 11:24:47 +0300

    sqs upd

--- src/Dex.MassTransit/Dex.MassTransit.SQS/AmazonMqOptions.cs
+++ src/Dex.MassTransit/Dex.MassTransit.SQS/AmazonMqOptions.cs
@@ -2,9 +2,9 @@ namespace Dex.MassTransit.Extensions.Options
 {
     public class AmazonMqOptions
     {
-        public string Region { get; set; }
-        public string AccessKey { get; set; }
-        public string SecretKey { get; set; }
-        public string OwnerId { get; set; }
+        public string? Region { get; set; }
+        public string? AccessKey { get; set; }
+        public string? SecretKey { get; set; }
+        public string? OwnerId { get; set; }
     }
 }
