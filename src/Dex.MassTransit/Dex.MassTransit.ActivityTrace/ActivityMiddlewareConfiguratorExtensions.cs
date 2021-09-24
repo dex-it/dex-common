2c855c971026caf45294a87284a4ebdd8c814ef0
mmx003 <mmx003@gmail.com>
2021-09-24 14:25:15 +0300

    minor fixes

--- src/Dex.MassTransit/Dex.MassTransit.ActivityTrace/ActivityMiddlewareConfiguratorExtensions.cs
+++ src/Dex.MassTransit/Dex.MassTransit.ActivityTrace/ActivityMiddlewareConfiguratorExtensions.cs
@@ -5,12 +5,12 @@ using MassTransit.Context;
 namespace Dex.MassTransit.ActivityTrace
 {
     /// <summary>
-    /// Add midleware to enable Activity tracing propagation for all consumers
+    /// Add midleware to enable Activity tracing propagation for all consumers.
     /// </summary>
     public static class ActivityMiddlewareConfiguratorExtensions
     {
         /// <summary>
-        /// Add ActivityTracing specification for IPipeSpecification[ConsumeContext]
+        /// Add ActivityTracing specification for IPipeSpecification[ConsumeContext].
         /// </summary>
         /// <param name="value"></param>
         /// <exception cref="ArgumentNullException"></exception>
