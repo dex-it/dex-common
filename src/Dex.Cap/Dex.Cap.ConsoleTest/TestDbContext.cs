b858482328caca9bc86ad4bc1c87088c3fc768ee
mmx003 <mmx003@gmail.com>
2022-09-18 16:46:11 +0300

    cosmetic fixes

--- src/Dex.Cap/Dex.Cap.ConsoleTest/TestDbContext.cs
+++ src/Dex.Cap/Dex.Cap.ConsoleTest/TestDbContext.cs
@@ -9,11 +9,6 @@ namespace Dex.Cap.ConsoleTest
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
