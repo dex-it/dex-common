2a400ad87c062758da0df6a67a7de260d090fce4
mmx003 <mmx003@gmail.com>
2022-10-06 12:15:29 +0300

    pass activity to MT header

---
+++ src/Dex.MassTransit/Dex.MassTransit.ActivityTrace/Filters/ActivityTracingSendFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace Dex.MassTransit.ActivityTrace.Filters
+{
+    internal class ActivityTracingSendFilter : IFilter<SendContext>, IFilter<PublishContext>
+    {
+        public async Task Send(SendContext context, IPipe<SendContext> next)
+        {
+            SetActivityHeader(context);
+            await next.Send(context).ConfigureAwait(false);
+        }
+
+        public async Task Send(PublishContext context, IPipe<PublishContext> next)
+        {
+            SetActivityHeader(context);
+            await next.Send(context).ConfigureAwait(false);
+        }
+
+        private static void SetActivityHeader(SendContext context)
+        {
+            if (Activity.Current?.Id != null)
+                context.Headers.Set(Consts.ActivityIdName, Activity.Current.Id);
+        }
+
+        public void Probe(ProbeContext context)
+        {
+        }
+    }
+}
