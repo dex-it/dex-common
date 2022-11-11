55a927ccc5e48dcc46697f072c07dcf78d5d8008
mmx003 <mmx003@gmail.com>
2022-11-12 00:17:41 +0200

    fix bug, TryLockMessageCore, select DbNow as must be UtcNow.

--- src/Dex.Cap/Tests/Dex.Cap.Ef.Tests/TestDbContext.cs
+++ src/Dex.Cap/Tests/Dex.Cap.Ef.Tests/TestDbContext.cs
@@ -2,7 +2,9 @@ using System;
 using Dex.Cap.Ef.Tests.Model;
 using Dex.Cap.OnceExecutor.Ef;
 using Dex.Cap.Outbox.Ef;
+using Dex.Cap.Outbox.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Logging;
 
 namespace Dex.Cap.Ef.Tests
@@ -22,7 +24,7 @@ namespace Dex.Cap.Ef.Tests
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            
+
             optionsBuilder.UseNpgsql($"Server=127.0.0.1;Port=5432;Database={_dbName};User Id=postgres;Password=my-pass~003;",
                 builder => { builder.EnableRetryOnFailure(); });
 
@@ -39,6 +41,18 @@ namespace Dex.Cap.Ef.Tests
 
             modelBuilder.OnceExecutorModelCreating();
             modelBuilder.OutboxModelCreating();
+            // modelBuilder.Entity<OutboxEnvelope>().
+        }
+
+        private sealed class DateTimeKindValueConverter : ValueConverter<DateTime, DateTime>
+        {
+            public DateTimeKindValueConverter(DateTimeKind kind, ConverterMappingHints mappingHints = null!)
+                : base(dateTime => dateTime.ToUniversalTime(), // Что-бы в timestamp дата всегда хранилась в UTC.
+                    dateTime => DateTime.SpecifyKind(dateTime, kind)
+                        .ToLocalTime(), // timestamp в базе эквивалентен `Unspecified DateTime` поэтому просто восстанавливаем заведомо известный Kind.
+                    mappingHints)
+            {
+            }
         }
     }
 }
