f600470381a99a39c60f6ef479fc5e40ffdf6f45
mmx <mmx003@gmail.com>
2022-12-16 12:38:49 +0200

    fux docs

--- src/Dex.Events/Dex.Events.Distributed/Extensions/MassTransitExtensions.cs
+++ src/Dex.Events/Dex.Events.Distributed/Extensions/MassTransitExtensions.cs
@@ -14,19 +14,23 @@ namespace Dex.Events.Distributed.Extensions
         /// <param name="configurator">IBusRegistrationConfigurator</param>
         /// <param name="registrationContext">IBusRegistrationContext</param>
         /// <param name="serviceName">Prefix for queue name</param>
-        /// <typeparam name="T">DistributedBaseEventParams</typeparam>
-        /// <typeparam name="TH1"></typeparam>
-        public static void SubscribeEventHandlers<T, TH1>(this IReceiveConfigurator<IReceiveEndpointConfigurator> configurator,
+        /// <typeparam name="TEventParams">DistributedBaseEventParams</typeparam>
+        /// <typeparam name="TDistributedEventHandler">Type of event handler</typeparam>
+        public static void SubscribeEventHandlers<TEventParams, TDistributedEventHandler>(this IReceiveConfigurator<IReceiveEndpointConfigurator> configurator,
             IBusRegistrationContext registrationContext, string? serviceName = null)
-            where T : DistributedBaseEventParams
-            where TH1 : IDistributedEventHandler<T>
+            where TEventParams : DistributedBaseEventParams
+            where TDistributedEventHandler : IDistributedEventHandler<TEventParams>
         {
             if (configurator == null) throw new ArgumentNullException(nameof(configurator));
             if (registrationContext == null) throw new ArgumentNullException(nameof(registrationContext));
 
             serviceName = serviceName ?? Assembly.GetCallingAssembly().GetName().Name;
-            var queueName = typeof(T).Name.ReplaceRegex("(?i)dto(?-i)$", "");
-            configurator.ReceiveEndpoint($"Event_{serviceName}_{queueName}_{typeof(TH1).Name}", x => x.ConfigureConsumer(registrationContext, typeof(TH1)));
+
+            var queueName = typeof(TEventParams).Name.ReplaceRegex("(?i)dto(?-i)$", "");
+            var consumerName = typeof(TDistributedEventHandler).Name.Replace("`", string.Empty, StringComparison.OrdinalIgnoreCase);
+            var fullName = $"Event_{serviceName}_{queueName}_{consumerName}";
+
+            configurator.ReceiveEndpoint(fullName, x => x.ConfigureConsumer(registrationContext, typeof(TDistributedEventHandler)));
         }
 
         /// <summary>
@@ -35,20 +39,21 @@ namespace Dex.Events.Distributed.Extensions
         /// <param name="configurator">IBusRegistrationConfigurator</param>
         /// <param name="registrationContext">IBusRegistrationContext</param>
         /// <param name="serviceName">Prefix for queue name</param>
-        /// <typeparam name="T">DistributedBaseEventParams</typeparam>
-        /// <typeparam name="TH1"></typeparam>
-        /// <typeparam name="TH2"></typeparam>
-        public static void SubscribeEventHandlers<T, TH1, TH2>(this IReceiveConfigurator<IReceiveEndpointConfigurator> configurator,
+        /// <typeparam name="TEventParams">DistributedBaseEventParams</typeparam>
+        /// <typeparam name="TDistributedEventHandler1">Type of event handler</typeparam>
+        /// <typeparam name="TDistributedEventHandler2">Type of event handler</typeparam>
+        public static void SubscribeEventHandlers<TEventParams, TDistributedEventHandler1, TDistributedEventHandler2>(
+            this IReceiveConfigurator<IReceiveEndpointConfigurator> configurator,
             IBusRegistrationContext registrationContext, string? serviceName = null)
-            where T : DistributedBaseEventParams
-            where TH1 : IDistributedEventHandler<T>
-            where TH2 : IDistributedEventHandler<T>
+            where TEventParams : DistributedBaseEventParams
+            where TDistributedEventHandler1 : IDistributedEventHandler<TEventParams>
+            where TDistributedEventHandler2 : IDistributedEventHandler<TEventParams>
         {
             if (configurator == null) throw new ArgumentNullException(nameof(configurator));
             if (registrationContext == null) throw new ArgumentNullException(nameof(registrationContext));
 
-            configurator.SubscribeEventHandlers<T, TH1>(registrationContext, serviceName);
-            configurator.SubscribeEventHandlers<T, TH2>(registrationContext, serviceName);
+            configurator.SubscribeEventHandlers<TEventParams, TDistributedEventHandler1>(registrationContext, serviceName);
+            configurator.SubscribeEventHandlers<TEventParams, TDistributedEventHandler2>(registrationContext, serviceName);
         }
 
         /// <summary>
@@ -57,23 +62,23 @@ namespace Dex.Events.Distributed.Extensions
         /// <param name="configurator">IBusRegistrationConfigurator</param>
         /// <param name="registrationContext">IBusRegistrationContext</param>
         /// <param name="serviceName">Prefix for queue name</param>
-        /// <typeparam name="T">DistributedBaseEventParams</typeparam>
-        /// <typeparam name="TH1"></typeparam>
-        /// <typeparam name="TH2"></typeparam>
-        /// <typeparam name="TH3"></typeparam>
-        public static void SubscribeEventHandlers<T, TH1, TH2, TH3>(this IReceiveConfigurator<IReceiveEndpointConfigurator> configurator,
-            IBusRegistrationContext registrationContext, string? serviceName = null)
-            where T : DistributedBaseEventParams
-            where TH1 : IDistributedEventHandler<T>
-            where TH2 : IDistributedEventHandler<T>
-            where TH3 : IDistributedEventHandler<T>
+        /// <typeparam name="TEventParams">DistributedBaseEventParams</typeparam>
+        /// <typeparam name="TDistributedEventHandler1">Type of event handler</typeparam>
+        /// <typeparam name="TDistributedEventHandler2">Type of event handler</typeparam>
+        /// <typeparam name="TDistributedEventHandler3">Type of event handler</typeparam>
+        public static void SubscribeEventHandlers<TEventParams, TDistributedEventHandler1, TDistributedEventHandler2, TDistributedEventHandler3>(
+            this IReceiveConfigurator<IReceiveEndpointConfigurator> configurator, IBusRegistrationContext registrationContext, string? serviceName = null)
+            where TEventParams : DistributedBaseEventParams
+            where TDistributedEventHandler1 : IDistributedEventHandler<TEventParams>
+            where TDistributedEventHandler2 : IDistributedEventHandler<TEventParams>
+            where TDistributedEventHandler3 : IDistributedEventHandler<TEventParams>
         {
             if (configurator == null) throw new ArgumentNullException(nameof(configurator));
             if (registrationContext == null) throw new ArgumentNullException(nameof(registrationContext));
 
-            configurator.SubscribeEventHandlers<T, TH1>(registrationContext, serviceName);
-            configurator.SubscribeEventHandlers<T, TH2>(registrationContext, serviceName);
-            configurator.SubscribeEventHandlers<T, TH3>(registrationContext, serviceName);
+            configurator.SubscribeEventHandlers<TEventParams, TDistributedEventHandler1>(registrationContext, serviceName);
+            configurator.SubscribeEventHandlers<TEventParams, TDistributedEventHandler2>(registrationContext, serviceName);
+            configurator.SubscribeEventHandlers<TEventParams, TDistributedEventHandler3>(registrationContext, serviceName);
         }
     }
 }
