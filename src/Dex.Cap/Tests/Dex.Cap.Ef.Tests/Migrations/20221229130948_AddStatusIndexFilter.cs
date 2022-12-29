a389dbdd0d7d0a63bb9f5ee552160222af469ad2
s.fetesko <s.fetesko@dextechnology.com>
2022-12-29 15:13:28 +0200

    add filter on status index only for unprocessed messages

---
+++ src/Dex.Cap/Tests/Dex.Cap.Ef.Tests/Migrations/20221229130948_AddStatusIndexFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dex.Cap.Ef.Tests.Migrations
+{
+    public partial class AddStatusIndexFilter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_outbox_Status",
+                schema: "cap",
+                table: "outbox");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_outbox_Status",
+                schema: "cap",
+                table: "outbox",
+                column: "Status",
+                filter: "\"Status\" in (0,1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_outbox_Status",
+                schema: "cap",
+                table: "outbox");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_outbox_Status",
+                schema: "cap",
+                table: "outbox",
+                column: "Status");
+        }
+    }
+}
