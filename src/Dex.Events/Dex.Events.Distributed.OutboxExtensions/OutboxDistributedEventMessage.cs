664b5a1d0ab330bdd1742a30b69a0f8fc8baee7d
mmx003 <mmx003@gmail.com>
2023-01-12 01:11:27 +0200

    fix for breaking change from outbox, add MessageId to OutboxMessage

--- src/Dex.Events/Dex.Events.Distributed.OutboxExtensions/OutboxDistributedEventMessage.cs
+++ src/Dex.Events/Dex.Events.Distributed.OutboxExtensions/OutboxDistributedEventMessage.cs
@@ -2,14 +2,14 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Dex.Cap.Outbox.Interfaces;
+using Dex.Cap.Outbox.Models;
 using Dex.Events.Distributed.Models;
 using MassTransit;
 
 namespace Dex.Events.Distributed.OutboxExtensions
 {
     [SuppressMessage("ReSharper", "UnusedTypeParameter")]
-    public sealed record OutboxDistributedEventMessage<TBus> : IOutboxMessage where TBus : IBus
+    public sealed class OutboxDistributedEventMessage<TBus> : BaseOutboxMessage where TBus : IBus
     {
         public string EventParams { get; }
         public string EventParamsType { get; }
