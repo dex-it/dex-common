6fb710018456d413180e8f5f3ebaa44ec5e5512e
mmx003 <mmx003@gmail.com>
2023-02-19 16:46:21 +0200

    test experiments

--- src/Dex.Cap/Tests/Dex.Cap.Ef.Tests/OutboxTests/TransactionScopeTests.cs
+++ src/Dex.Cap/Tests/Dex.Cap.Ef.Tests/OutboxTests/TransactionScopeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
 using Dex.Cap.Common.Ef.Helpers;
@@ -129,46 +130,6 @@ namespace Dex.Cap.Ef.Tests.OutboxTests
         }
 
         [Test]
-        public void Ambient_CommitableTransactionTest1()
-        {
-            TestDbContext.IsRetryStrategy = false;
-
-            var sp = InitServiceCollection()
-                .BuildServiceProvider();
-
-            var id = Guid.NewGuid();
-            using (var aScope = new CommittableTransaction(new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted }))
-            {
-                Transaction.Current = aScope;
-
-                var db = sp.CreateScope().ServiceProvider.GetRequiredService<TestDbContext>();
-                db.Database.CreateExecutionStrategy()
-                    .Execute(0,
-                        (context, i) =>
-                        {
-                            using (var inner2 = TransactionScopeHelper
-                                       .CreateTransactionScope(TransactionScopeOption.RequiresNew, IsolationLevel.ReadCommitted))
-                            {
-                                db.Users.Add(new TestUser { Id = id, Name = "max" });
-                                db.SaveChanges();
-
-                                inner2.Complete();
-                            }
-
-                            return 0;
-                        },
-                        (context, i) => new ExecutionResult<int>(true, 0));
-
-                aScope.Rollback();
-                Transaction.Current = null;
-            }
-
-            var db2 = sp.GetRequiredService<TestDbContext>();
-            var saved = db2.Users.Find(id);
-            Assert.IsNotNull(saved);
-        }
-
-        [Test]
         public void Ambient_2Transaction_Complete_UncompleteTest1()
         {
             TestDbContext.IsRetryStrategy = false;
@@ -214,7 +175,7 @@ namespace Dex.Cap.Ef.Tests.OutboxTests
                             return 0;
                         },
                         (context, i) => new ExecutionResult<int>(true, 0));
-                
+
                 ambient.Complete();
             }
 
@@ -224,6 +185,87 @@ namespace Dex.Cap.Ef.Tests.OutboxTests
 
             var saved2 = db2.Users.Find(id2);
             Assert.IsNull(saved2);
+        }
+
+        // regular transaction
+
+        [Test]
+        public void Ambient_CommitableTransactionTest1()
+        {
+            TestDbContext.IsRetryStrategy = false;
+
+            var sp = InitServiceCollection()
+                .BuildServiceProvider();
+
+            var id = Guid.NewGuid();
+            using (var aScope = new CommittableTransaction(new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted }))
+            {
+                Transaction.Current = aScope;
+
+                var db = sp.CreateScope().ServiceProvider.GetRequiredService<TestDbContext>();
+                db.Database.CreateExecutionStrategy()
+                    .Execute(0,
+                        (context, i) =>
+                        {
+                            using (var inner2 = TransactionScopeHelper
+                                       .CreateTransactionScope(TransactionScopeOption.RequiresNew, IsolationLevel.ReadCommitted))
+                            {
+                                db.Users.Add(new TestUser { Id = id, Name = "max" });
+                                db.SaveChanges();
+
+                                inner2.Complete();
+                            }
+
+                            return 0;
+                        },
+                        (context, i) => new ExecutionResult<int>(true, 0));
+
+                aScope.Rollback();
+                Transaction.Current = null;
+            }
+
+            var db2 = sp.GetRequiredService<TestDbContext>();
+            var saved = db2.Users.Find(id);
+            Assert.IsNotNull(saved);
+        }
+
+        [Ignore("experiments")]
+        public async Task Ambient_2InnerTransactionTest()
+        {
+            TestDbContext.IsRetryStrategy = false;
+            var sp = InitServiceCollection().BuildServiceProvider();
+
+            using (var ambient = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+            {
+                using var scope1 = sp.CreateScope();
+                await using (var db1 = scope1.ServiceProvider.GetRequiredService<TestDbContext>())
+                {
+                    await db1.Database.BeginTransactionAsync();
+
+                    db1.Users.Add(new TestUser { Name = "mmx1" });
+                    await db1.SaveChangesAsync();
+
+                    await db1.Database.CommitTransactionAsync();
+                }
+
+                using var scope2 = sp.CreateScope();
+                await using (var db2 = scope2.ServiceProvider.GetRequiredService<TestDbContext>())
+                {
+                    await db2.Database.BeginTransactionAsync();
+
+                    db2.Users.Add(new TestUser { Name = "mmx2" });
+                    await db2.SaveChangesAsync();
+
+                    await db2.Database.CommitTransactionAsync();
+                }
+
+                //ambient.Complete(); // cancell changes
+            }
+
+            using var scope3 = sp.CreateScope();
+            var db3 = scope3.ServiceProvider.GetRequiredService<TestDbContext>();
+            // Assert.AreEqual(2, db3.Users.Count());
+            Assert.AreEqual(0, db3.Users.Count());
         }
     }
 }
