90e16f4f1e27f71d5ea289eb5d17b1d9d5997981
Бербат Георгий <g.berbat@dextechnology.com>
2023-09-25 16:09:15 +0300

    AT-250 feat: add in memory once executor

---
+++ src/Dex.Cap/Dex.Cap.OnceExecutor.Memory/Extensions/MicrosoftDependencyInjectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Dex.Cap.OnceExecutor.Memory.Extensions;
+
+public static class MicrosoftDependencyInjectionExtensions
+{
+    public static IServiceCollection AddOnceExecutor<TDistributedCache>(this IServiceCollection services,
+        Action<MemoryDistributedCacheOptions> globalCacheOptions, Action<DistributedCacheEntryOptions> entryOptions)
+        where TDistributedCache : class, IDistributedCache
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        services.AddOptions();
+        services.Configure(entryOptions);
+        services.Configure(globalCacheOptions);
+        services.TryAdd(ServiceDescriptor.Singleton<TDistributedCache, TDistributedCache>());
+
+        return services
+            .AddScoped(typeof(IOnceExecutor<IMemoryOptions, IDistributedCache>), typeof(OnceExecutorMemory<TDistributedCache>))
+            .AddScoped(typeof(IOnceExecutor<IMemoryOptions>), typeof(OnceExecutorMemory<TDistributedCache>));
+    }
+}
