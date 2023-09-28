3b6a6d40285df1c7b6516a863f2ebf481ed5373a
Anatoly Juk™ Sirik <44912569+juk-777@users.noreply.github.com>
2023-09-28 13:43:31 +0300

    add in memory once executor, fix usings, package params (#139)
    * AT-250 feat: add in memory once executor (#138)
    
    Co-authored-by: Бербат Георгий <g.berbat@dextechnology.com>
    Co-authored-by: a.sirik <ant7juk@gmail.com>
    (cherry picked from commit 50f00597cddb8ecb6ae7cd7ce463139aa107ef1d)
    
    * fix usings, package params
    
    ---------
    
    Co-authored-by: George B <113766655+georgeberbat@users.noreply.github.com>

---
+++ src/Dex.Cap/Dex.Cap.OnceExecutor.Memory/Extensions/MicrosoftDependencyInjectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Dex.Cap.OnceExecutor.Memory.Extensions
+{
+    public static class MicrosoftDependencyInjectionExtensions
+    {
+        public static IServiceCollection AddOnceExecutor<TDistributedCache>(this IServiceCollection services,
+            Action<MemoryDistributedCacheOptions> globalCacheOptions, Action<DistributedCacheEntryOptions> entryOptions)
+            where TDistributedCache : class, IDistributedCache
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.Configure(entryOptions);
+            services.Configure(globalCacheOptions);
+            services.TryAdd(ServiceDescriptor.Singleton<TDistributedCache, TDistributedCache>());
+
+            return services
+                .AddScoped(typeof(IOnceExecutor<IOnceExecutorMemoryOptions, IDistributedCache>), typeof(OnceExecutorMemory<TDistributedCache>))
+                .AddScoped(typeof(IOnceExecutor<IOnceExecutorMemoryOptions>), typeof(OnceExecutorMemory<TDistributedCache>));
+        }
+    }
+}
