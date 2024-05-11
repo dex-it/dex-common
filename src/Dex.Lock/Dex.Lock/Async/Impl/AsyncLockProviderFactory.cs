54abf4351d5468165fe5f81526dc5f499a0a0375
Max Tonkoglas <mmx003@gmail.com>
2024-05-11 16:15:17 +0300

    Nuget upgrade (main) (#146)
    * upgrade nugets Dex
    
    * upgrade nugets Dex.Cache
    
    * upgrade nugets Dex.Cap
    
    * Dex.Cap inc. version
    
    * add Directory.Build.targets
    inc. versions to Dex.Cap
    
    * upgrade Dex.Events
    
    * upgrage Dex.MassTransit
    set Masstransit version 8.2.2
    
    * upgrade Dex.SecurityTokenProvider
    
    * fix version MassTransit.TestFramework
    
    * switch version to 6.0

--- src/Dex.Lock/Dex.Lock/Async/Impl/AsyncLockProviderFactory.cs
+++ src/Dex.Lock/Dex.Lock/Async/Impl/AsyncLockProviderFactory.cs
@@ -2,7 +2,7 @@ namespace Dex.Lock.Async.Impl
 {
     public class AsyncLockProviderFactory : IAsyncLockProviderFactory<LockReleaser>
     {
-        public IAsyncLockProvider<T, LockReleaser> Create<T>(string? instanceId)
+        public IAsyncLockProvider<T, LockReleaser> Create<T>(string? instanceId) where T : notnull
         {
             return new AsyncLockProvider<T>();
         }
