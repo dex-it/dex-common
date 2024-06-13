ee959b1127f57994f22e5669238ff4938050d700
yanchev.k <yanchev.k@ct-sg.ru>
2024-06-13 17:16:09 +0300

    dex.audit: fix: value objects сравниваются корректно, интерцепторы вынесены в отдельный проект

--- src/Dex.Audit/Dex.Audit.Domain/ValueObjects/Device.cs
+++ src/Dex.Audit/Dex.Audit.Domain/ValueObjects/Device.cs
@@ -8,25 +8,53 @@ public class Device
     /// <summary>
     /// Название АС.
     /// </summary>
-    public string? Vendor { get; set; }
+    public string? Vendor { get; init; }
 
     /// <summary>
     /// Версия АС.
     /// </summary>
-    public string? Version { get; set; }
+    public string? Version { get; init; }
 
     /// <summary>
     /// Название журнала аудита АС.
     /// </summary>
-    public string? Product { get; set; }
+    public string? Product { get; init; }
     
     /// <summary>
     /// Полное имя процесса (службы).
     /// </summary>
-    public string? ProcessName { get; set; }
+    public string? ProcessName { get; init; }
     
     /// <summary>
     /// Системный идентификатор сообщения о событии.
     /// </summary>
-    public long? EventClassId { get; set; }
+    public long? EventClassId { get; init; }
+
+    /// <summary>
+    /// Метод сравнения объектов.
+    /// </summary>
+    /// <param name="obj">Входной объект.</param>
+    /// <returns>true, если объекты равны, иначе false.</returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Device device)
+        {
+            return false;
+        }
+
+        return Vendor == device.Vendor &&
+               Version == device.Version && 
+               Product == device.Product && 
+               ProcessName == device.ProcessName &&
+               EventClassId == device.EventClassId;
+    }
+
+    /// <summary>
+    /// Метод получения хеш-кода объекта.
+    /// </summary>
+    /// <returns>Хеш-код на основе свойтв объекта.</returns>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Vendor, Version, Product, ProcessName, EventClassId);
+    }
 }
