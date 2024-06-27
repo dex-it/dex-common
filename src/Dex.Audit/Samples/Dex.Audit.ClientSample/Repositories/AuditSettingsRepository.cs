b9396cc2a979df78a306229d59f93d325152bdae
yanchev.k <yanchev.k@ct-sg.ru>
2024-06-27 11:24:34 +0300

    dex.audit: feat: добавлен пример с логгером

---
+++ src/Dex.Audit/Samples/Dex.Audit.ClientSample/Repositories/AuditSettingsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dex.Audit.Client.Interfaces;
+using Dex.Audit.Domain.Entities;
+
+namespace Dex.Audit.ClientSample.Repositories;
+
+public class AuditSettingsRepository : IAuditSettingsRepository
+{
+    public async Task<AuditSettings?> GetAsync(string eventType, CancellationToken cancellationToken = default)
+    {
+        return new AuditSettings();
+    }
+
+    public Task AddAsync(string settingEventType, AuditSettings setting, TimeSpan refreshInterval,
+        CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+}
