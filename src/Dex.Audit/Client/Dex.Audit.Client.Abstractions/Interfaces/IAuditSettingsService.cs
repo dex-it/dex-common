2423d24b4cefadcbf6b1bd43162afad577292f10
yanchev.k <yanchev.k@ct-sg.ru>
2024-08-13 21:47:23 +0300

    dex.audit: feature: grpc реализован для клиента

---
+++ src/Dex.Audit/Client/Dex.Audit.Client.Abstractions/Interfaces/IAuditSettingsService.cs
@@ -0,0 +1,8 @@
+﻿using Dex.Audit.Domain.Entities;
+
+namespace Dex.Audit.Client.Abstractions.Interfaces;
+
+public interface IAuditSettingsService
+{
+    Task<AuditSettings?> GetOrGetAndUpdateSettingsAsync(string eventType, CancellationToken cancellationToken = default);
+}
