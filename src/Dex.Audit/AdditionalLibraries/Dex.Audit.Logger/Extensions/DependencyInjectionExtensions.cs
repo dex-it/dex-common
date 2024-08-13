c98b01934caa0093685814fddba5fda9824bd760
yanchev.k <yanchev.k@ct-sg.ru>
2024-08-13 22:18:16 +0300

    dex.audit: refactor: дополнительные библиотеки вынесены в отдельные папки

--- src/Dex.Audit/AdditionalLibraries/Dex.Audit.Logger/Extensions/DependencyInjectionExtensions.cs
+++ src/Dex.Audit/AdditionalLibraries/Dex.Audit.Logger/Extensions/DependencyInjectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Dex.Audit.Logger.Options;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -11,19 +10,19 @@ public static class DependencyInjectionExtensions
     /// Добавить аудируемые логи.
     /// </summary>
     /// <param name="builder"><see cref="ILoggingBuilder"/>.</param>
-    /// <param name="configuration"><see cref="IConfiguration"/></param>
     /// <param name="dispose">Освобождать ли ресурсы средствами DI.</param>
     /// <returns></returns>
     public static ILoggingBuilder AddAuditLogger(
         this ILoggingBuilder builder,
-        IConfiguration configuration,
         bool dispose = false)
     {
         ArgumentNullException.ThrowIfNull(builder);
 
+        var services = builder.Services;
+
         if (dispose)
         {
-            builder.Services.AddSingleton<ILoggerProvider, AuditLoggerProvider>(_ => new AuditLoggerProvider());
+            services.AddSingleton<ILoggerProvider, AuditLoggerProvider>(_ => new AuditLoggerProvider());
         }
         else
         {
@@ -32,10 +31,9 @@ public static class DependencyInjectionExtensions
 
         builder.AddFilter<AuditLoggerProvider>(_ => true).SetMinimumLevel(LogLevel.Trace);
 
-        builder.Services.AddHostedService<AuditLoggerReader>();
+        services.AddHostedService<AuditLoggerReader>();
 
-        builder.Services.Configure<AuditLoggerOptions>(opts =>
-            configuration.GetSection(nameof(AuditLoggerOptions)).Bind(opts));
+        services.AddOptions<AuditLoggerOptions>(nameof(AuditLoggerOptions));
 
         return builder;
     }
