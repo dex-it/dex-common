bb9015f5d2b783f07d0ec00fe36af93438bf619e
yanchev.k <yanchev.k@ct-sg.ru>
2024-08-20 14:43:57 +0300

    dex.audit: fix: сериализация ответа через newtonsoft

--- src/Dex.Audit/Tests/Dex.Audit.Sample.Client/Application/Services/ClientAuditSettingsService.cs
+++ src/Dex.Audit/Tests/Dex.Audit.Sample.Client/Application/Services/ClientAuditSettingsService.cs
@@ -1,9 +1,14 @@
 ﻿using Dex.Audit.Client.Abstractions.Interfaces;
 using Dex.Audit.Domain.Entities;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace Dex.Audit.ClientSample.Application.Services;
 
-public class ClientAuditSettingsService(IAuditCacheRepository cacheRepository, IConfiguration configuration) : IAuditSettingsService
+public class ClientAuditSettingsService(
+    IAuditCacheRepository cacheRepository,
+    IConfiguration configuration,
+    ILogger<ClientAuditSettingsService> logger) : IAuditSettingsService
 {
     public async Task<AuditSettings?> GetOrGetAndUpdateSettingsAsync(string eventType, CancellationToken cancellationToken = default)
     {
@@ -14,9 +19,8 @@ public class ClientAuditSettingsService(IAuditCacheRepository cacheRepository, I
             return setting;
         }
 
-        using var httpClient = new HttpClient();
-        using var result = await httpClient.GetAsync(configuration.GetConnectionString("ServerAddress") + "/Settings", cancellationToken);
-        var settings = await result.Content.ReadFromJsonAsync<IEnumerable<AuditSettings>>(cancellationToken: cancellationToken);
+        var settings = await GetSettingsFromServerAsync(cancellationToken);
+
         if (settings == null)
         {
             return null;
@@ -27,5 +31,30 @@ public class ClientAuditSettingsService(IAuditCacheRepository cacheRepository, I
             .ConfigureAwait(false);
 
         return await cacheRepository.GetAsync(eventType, cancellationToken);
+    }
+
+    private async Task<IEnumerable<AuditSettings>?> GetSettingsFromServerAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            using var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            using var httpClient = new HttpClient(handler);
+            using var result =
+                await httpClient.GetAsync(
+                    configuration.GetConnectionString("ServerSettingsAddress"),
+                    cancellationToken);
+
+            var serializer = new JsonSerializer();
+            using var sr = new StreamReader(await result.Content.ReadAsStreamAsync(cancellationToken));
+            using var jsonTextReader = new JsonTextReader(sr);
+            return serializer.Deserialize<IEnumerable<AuditSettings>?>(jsonTextReader);
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, exception.Message);
+            return null;
+        }
     }
 }
