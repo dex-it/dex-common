798b881ceb3b0c15c1b0de1429d256968a4ae0ad
yanchev.k <yanchev.k@ct-sg.ru>
2024-08-26 10:36:10 +0300

    dex.audit: refactor: подключения аудируемого MediatR через расширение

---
+++ src/Dex.Audit/AdditionalLibraries/Dex.Audit.MediatR/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Dex.Audit.MediatR.PipelineBehaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dex.Audit.MediatR.Extensions;
+
+/// <summary>
+/// Статический класс, который содержит методы расширения для конфигурации поведения конвейера MeiatR.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Добавить в конвейер аудируемость.
+    /// </summary>
+    /// <param name="configuration"><see cref="MediatRServiceConfiguration"/></param>
+    public static void AddPipelineAuditBehavior(this MediatRServiceConfiguration configuration)
+    {
+        configuration.AddBehavior(typeof(IPipelineBehavior<,>),typeof(AuditBehavior<,>));
+    }
+}
