9f94a2977f563621acfeae5d72a614caae1a9433
елизавета чабан <e.chaban@dextechnology.com>
2025-01-21 18:16:56 +0300

    dex.configuration: Добавлены проекты для защиты данных конфигураций

---
+++ src/Dex.Configuration/Dex.Configuration.Dataprotection.Cli/Dex.Configuration.DataProtection/ApplicationNames/TemplateApplicationName.cs
@@ -0,0 +1,10 @@
+﻿namespace Dex.Configuration.DataProtection.ApplicationNames
+{
+    /// <summary>
+    /// Названия проектов.
+    /// </summary>
+    public enum TemplateApplicationName
+    {
+        AuditWriter = 1
+    }
+}
---
+++ src/Dex.Configuration/Dex.Configuration.ProtectedJson/Dex.Configuration.ProtectedJson/CreateDataProtectorDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace Dex.Configuration.ProtectedJson
+{
+    /// <summary>
+    /// Делегат создания <see cref="IDataProtector"/>.
+    /// </summary>
+    public delegate IDataProtector CreateDataProtectorDelegate(string applicationName, string keysDirectory);
+}
