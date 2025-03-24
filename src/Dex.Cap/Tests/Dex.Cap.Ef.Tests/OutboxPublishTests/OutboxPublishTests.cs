c3470de680cb609a5c885727227949e87471a8fc
Anatoly Juk™ Sirik <44912569+juk-777@users.noreply.github.com>
2025-03-24 11:04:32 +0200

    fix IOutboxMessage: make property MessageId get-only (#172)
    * fix IOutboxMessage: make property MessageId get-only
    
    * add explicit messageId to command in SerializerTest

---
+++ src/Dex.Cap/Tests/Dex.Cap.Ef.Tests/OutboxPublishTests/OutboxPublishTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Dex.Cap.Outbox.Interfaces;
+using Dex.Cap.Outbox.OnceExecutor.MassTransit;
+using Dex.Outbox.Command.Test;
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Dex.Cap.Ef.Tests.OutboxPublishTests
+{
+    public class OutboxPublishTests : BaseTest
+    {
+        [Test]
+        public async Task EnqueueAndPublishMessageUsingTestHarness_SuccessTrue()
+        {
+            var sp = InitServiceCollection()
+                .AddScoped(typeof(IOutboxMessageHandler<>), typeof(PublisherOutboxHandler<>))
+                .AddMassTransitTestHarness()
+                .BuildServiceProvider();
+
+            var outboxService = sp.GetRequiredService<IOutboxService>();
+            var harness = sp.GetRequiredService<ITestHarness>();
+            await harness.Start();
+
+            var correlationId = Guid.NewGuid();
+            var command = new TestOutboxCommand { Args = "hello world" };
+            await outboxService.EnqueueAsync(correlationId, command);
+            await SaveChanges(sp);
+
+            var handler = sp.GetRequiredService<IOutboxHandler>();
+            await handler.ProcessAsync(CancellationToken.None);
+
+            Assert.IsTrue(await harness.Published.Any<TestOutboxCommand>());
+            await harness.Stop();
+        }
+    }
+}
