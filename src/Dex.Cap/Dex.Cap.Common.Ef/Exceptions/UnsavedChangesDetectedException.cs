4b1b2975865636138fe52012643416b247b26213
a.sirik <ant7juk@gmail.com>
2025-05-07 16:45:11 +0300

    Rework UnsavedChangesDetectedException

--- src/Dex.Cap/Dex.Cap.Common.Ef/Exceptions/UnsavedChangesDetectedException.cs
+++ src/Dex.Cap/Dex.Cap.Common.Ef/Exceptions/UnsavedChangesDetectedException.cs
@@ -6,6 +6,28 @@ namespace Dex.Cap.Common.Ef.Exceptions
 {
     public sealed class UnsavedChangesDetectedException : Exception
     {
+        private const string UnsavedChangesCount = "unsaved_changes_count";
+        private const string UnsavedChangesFirst10 = "unsaved_changes_first_10";
+
+        public override string Message
+        {
+            get
+            {
+                var unsavedChangesCount = Data.Contains(UnsavedChangesCount) ? Data[UnsavedChangesCount] : null;
+                var unsavedChangesFirst10 = Data.Contains(UnsavedChangesFirst10) ? Data[UnsavedChangesFirst10] : null;
+
+                var unsavedChangesCountString = unsavedChangesCount is null
+                    ? string.Empty
+                    : $" unsavedChangesCount: {unsavedChangesCount}";
+
+                var unsavedChangesFirst10String = unsavedChangesFirst10 is null
+                    ? string.Empty
+                    : $" unsavedChangesFirst10: {unsavedChangesFirst10}";
+
+                return base.Message + unsavedChangesCountString + unsavedChangesFirst10String;
+            }
+        }
+
         public UnsavedChangesDetectedException()
         {
         }
@@ -26,8 +48,8 @@ namespace Dex.Cap.Common.Ef.Exceptions
                 .Where(e => e.State != EntityState.Unchanged)
                 .Select(e => e.Entity.GetType()).ToArray();
 
-            Data["unsaved_changes_count"] = entries.Length;
-            Data["unsaved_changes_first_10"] = string.Join("; ",entries.Take(10));
+            Data[UnsavedChangesCount] = entries.Length;
+            Data[UnsavedChangesFirst10] = string.Join("; ", entries.Take(10));
         }
     }
 }
