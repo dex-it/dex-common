870f3975ab87eaf70a6e2ffa372678ddd5c713e8
a.sirik <ant7juk@gmail.com>
2025-05-13 15:10:40 +0300

    add default values to Page/PageSize

--- src/Dex.Pagination/Conditions/PageCondition.cs
+++ src/Dex.Pagination/Conditions/PageCondition.cs
@@ -1,21 +1,48 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Dex.Pagination.Conditions
 {
     public record PageCondition : IPageCondition
     {
+        private readonly int _page = 1;
+        private readonly int _pageSize = 10;
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "External usage")]
+        public PageCondition()
+        {
+        }
+
         public PageCondition(int page, int pageSize)
         {
-            if (page < 1)
-                throw new ArgumentOutOfRangeException(nameof(page), "must be greater 0");
-            if (pageSize < 1)
-                throw new ArgumentOutOfRangeException(nameof(pageSize), "must be greater 0");
-
             Page = page;
             PageSize = pageSize;
         }
 
-        public int Page { get; }
-        public int PageSize { get; }
+        //todo После обновления до .net10 использовать ключевое слово field
+        public int Page
+        {
+            get => _page;
+            init
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException(nameof(Page), "must be greater 0");
+
+                _page = value;
+            }
+        }
+
+        //todo После обновления до .net10 использовать ключевое слово field
+        public int PageSize
+        {
+            get => _pageSize;
+            init
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException(nameof(PageSize), "must be greater 0");
+
+                _pageSize = value;
+            }
+        }
     }
 }
