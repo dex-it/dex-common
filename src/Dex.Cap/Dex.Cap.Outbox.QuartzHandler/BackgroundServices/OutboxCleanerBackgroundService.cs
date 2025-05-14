e99a5744d47a3df2ee77400bd23665c4f47faaf7
Anatoly Juk™ Sirik <44912569+juk-777@users.noreply.github.com>
2025-05-14 14:48:06 +0300

    async/await rework Scheduler (#186)
    (cherry picked from commit a9bd4644b5cb11d5c2b4779479e15dc7578b4cbf)

--- src/Dex.Cap/Dex.Cap.Outbox.QuartzHandler/BackgroundServices/OutboxCleanerBackgroundService.cs
+++ src/Dex.Cap/Dex.Cap.Outbox.QuartzHandler/BackgroundServices/OutboxCleanerBackgroundService.cs
@@ -75,11 +75,11 @@ namespace Dex.Cap.Outbox.AspNetScheduler.BackgroundServices
             }
         }
 
-        private async Task InitDelay(CancellationToken cancellationToken)
+        private Task InitDelay(CancellationToken cancellationToken)
         {
             var initDelay = TimeSpan.FromMilliseconds(RandomNumberGenerator.GetInt32(20_000, 40_000));
             _logger.LogDebug("Initial delay for {Seconds} seconds to solve split brain problem", (int)initDelay.TotalSeconds);
-            await Task.Delay(initDelay, cancellationToken).ConfigureAwait(false);
+            return Task.Delay(initDelay, cancellationToken);
         }
     }
 }
