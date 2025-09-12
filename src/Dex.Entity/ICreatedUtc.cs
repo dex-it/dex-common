b58cd589fbaa39974dfd752177b22824a05f8e88
Виталий Чабан <v.chaban@dextechnology.com>
2025-09-12 19:53:05 +0300

    init in created utc

--- src/Dex.Entity/ICreatedUtc.cs
+++ src/Dex.Entity/ICreatedUtc.cs
@@ -4,6 +4,10 @@ namespace Dex.Entity
 {
     public interface ICreatedUtc
     {
+#if NET8_0_OR_GREATER
+        DateTime CreatedUtc { get; init; }
+#else
         DateTime CreatedUtc { get; set; }
+#endif
     }
 }
