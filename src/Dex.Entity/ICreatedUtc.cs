d785b412641432f8c6a404435d059f2b3c03b9c2
Виталий Чабан <v.chaban@dextechnology.com>
2025-09-12 16:01:20 +0300

    dotnet update

--- src/Dex.Entity/ICreatedUtc.cs
+++ src/Dex.Entity/ICreatedUtc.cs
@@ -4,6 +4,10 @@ namespace Dex.Entity
 {
     public interface ICreatedUtc
     {
-        DateTime CreatedUtc { get; }
+#if NET8_0_OR_GREATER
+        DateTime CreatedUtc { get; init; }
+#else
+        DateTime CreatedUtc { get; set; }
+#endif
     }
 }
