44cca071b936290e1afb0c7d7e5c687ffa8a17fd
VitaliyChaban <95292018+VitaliyChaban@users.noreply.github.com>
2025-09-15 19:02:17 +0300

    set for created utc + update internal dependent versions (#198)
    * set for created utc + update internal dependent versions
    
    * Dex.Extensions version fix

--- src/Dex.Entity/ICreatedUtc.cs
+++ src/Dex.Entity/ICreatedUtc.cs
@@ -4,10 +4,6 @@ namespace Dex.Entity
 {
     public interface ICreatedUtc
     {
-#if NET8_0_OR_GREATER
-        DateTime CreatedUtc { get; init; }
-#else
         DateTime CreatedUtc { get; set; }
-#endif
     }
 }
