bf7abe1790fa567c94eba36f33f4c53262be4635
Виталий Чабан <v.chaban@dextechnology.com>
2026-02-11 15:47:07 +0200

    add IOutboxEnvelopFactory

---
+++ src/Dex.Cap/Dex.Cap.Outbox/Interfaces/IOutboxEnvelopFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Dex.Cap.Common.Interfaces;
+using Dex.Cap.Outbox.Models;
+
+namespace Dex.Cap.Outbox.Interfaces;
+
+public interface IOutboxEnvelopFactory
+{
+    public OutboxEnvelope CreateEnvelop<T>(T message, Guid correlationId, DateTime? startAtUtc, TimeSpan? lockTimeout) where T : class, IOutboxMessage;
+}
---
+++ src/Dex.Cap/Dex.Cap.Outbox/OutboxEnvelopFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dex.Cap.Common.Interfaces;
+using Dex.Cap.Outbox.Exceptions;
+using Dex.Cap.Outbox.Interfaces;
+using Dex.Cap.Outbox.Models;
+
+namespace Dex.Cap.Outbox;
+
+internal sealed class OutboxEnvelopFactory(IOutboxSerializer serializer, IOutboxTypeDiscriminatorProvider discriminatorProvider) : IOutboxEnvelopFactory
+{
+    public OutboxEnvelope CreateEnvelop<T>(T message, Guid correlationId, DateTime? startAtUtc, TimeSpan? lockTimeout) where T : class, IOutboxMessage
+    {
+        var supportedDiscriminators = discriminatorProvider.CurrentDomainOutboxMessageTypes;
+
+        if (supportedDiscriminators.ContainsKey(T.OutboxTypeId) is false)
+            throw new DiscriminatorResolveException($"Сообщение {T.OutboxTypeId} не найдено в данном сервисе");
+
+        var messageType = message.GetType();
+        var envelopeId = Guid.NewGuid();
+
+        var msgBody = serializer.Serialize(messageType, message);
+        var outboxEnvelope = new OutboxEnvelope(envelopeId, correlationId, T.OutboxTypeId, msgBody, startAtUtc, lockTimeout);
+
+        return outboxEnvelope;
+    }
+}
