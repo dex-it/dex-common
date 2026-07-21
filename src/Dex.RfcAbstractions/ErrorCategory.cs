6011d87f8ee083da99688919c06b799ac552a401
a.sirik <a.sirik@dextechnology.com>
2026-07-21 22:34:34 +0300

    Убрать пробелы

--- src/Dex.RfcAbstractions/ErrorCategory.cs
+++ src/Dex.RfcAbstractions/ErrorCategory.cs
@@ -5,19 +5,19 @@
 /// </summary>
 public enum ErrorCategory
 {
-    Unknown = 0,        // -> 500
-    Validation,         // -> 400 (совместимость; НЕ 422)
-    BadRequest,         // -> 400
-    UserError,          // -> 400
-    Unauthorized,       // -> 401
-    Forbidden,          // -> 403
-    NotFound,           // -> 404
-    Conflict,           // -> 409
-    AlreadyExists,      // -> 409
+    Unknown = 0, // -> 500
+    Validation, // -> 400 (совместимость; НЕ 422)
+    BadRequest, // -> 400
+    UserError, // -> 400
+    Unauthorized, // -> 401
+    Forbidden, // -> 403
+    NotFound, // -> 404
+    Conflict, // -> 409
+    AlreadyExists, // -> 409
     PreconditionFailed, // -> 412
-    PaymentRequired,    // -> 402
-    TooManyRequests,    // -> 429
-    Timeout,            // -> 408
-    IntegrationError,   // -> 412
-    ServiceUnavailable  // -> 503
+    PaymentRequired, // -> 402
+    TooManyRequests, // -> 429
+    Timeout, // -> 408
+    IntegrationError, // -> 412
+    ServiceUnavailable // -> 503
 }
